958348948c872b0f58d29d8dae0239f6a1edf285
Adrian Garcia Badaracco <1755071+adriangb@users.noreply.github.com>
2024-06-17 14:44:53 -0500

    reliable signals test

--- tests/test_signal_handling.py
+++ tests/test_signal_handling.py
@@ -1,26 +1,19 @@
 from __future__ import annotations
 
-from multiprocessing import Process, Queue
+from contextlib import AsyncExitStack, asynccontextmanager
 import os
+from pathlib import Path
 import signal
 import socket
+import sys
+from tempfile import TemporaryDirectory
+from typing import AsyncIterator
 
 import anyio
+import anyio.abc
 import anyio.to_thread
+import httpx
 import pytest
-from asapi import serve
-from fastapi import FastAPI
-
-
-async def main(logger: Queue[str], port: int) -> None:
-    logger.put("start")
-    app = FastAPI()
-    await serve(app, port)
-    logger.put("stop")
-
-
-def app_subprocess(logger: Queue[str], port: int) -> None:
-    anyio.run(main, logger, port)
 
 
 def get_random_port() -> int:
@@ -29,21 +22,55 @@ def get_random_port() -> int:
         return s.getsockname()[1]
 
 
+@asynccontextmanager
+async def cleanup_process(process: anyio.abc.Process) -> AsyncIterator[None]:
+    try:
+        yield
+    except Exception as e:
+        print(e)
+        raise
+    finally:
+        if process.returncode is not None:
+            return
+        process.kill()
+        await process.wait()
+
+
+async def wait_for_server(port: int) -> None:
+    async with httpx.AsyncClient() as client:
+        while True:
+            await anyio.sleep(0.05)
+            try:
+                resp = await client.get(f"http://localhost:{port}/docs")
+                if resp.status_code == 200:
+                    break
+            except httpx.NetworkError:
+                pass
+
+
 @pytest.mark.anyio
 async def test_serve_stop_sigint() -> None:
-    q: Queue[str] = Queue(maxsize=5)
-    process = Process(target=app_subprocess, args=(q, get_random_port()))
-    process.start()
-    assert q.get() == "start"
-    # give the server enough time to actually start up
-    # if not we should still shut down but there might be some errors because of task cancellation
-    # and such
-    await anyio.sleep(0.5)
-    pid = process.pid
-    assert pid is not None
-    os.kill(pid, signal.SIGINT)
-    with anyio.fail_after(1):
-        await anyio.to_thread.run_sync(process.join)
-        assert process.exitcode == 0
-        r = await anyio.to_thread.run_sync(q.get)
-        assert r == "stop"
+    port = get_random_port()
+
+    async with AsyncExitStack() as stack:
+        td = stack.enter_context(TemporaryDirectory())
+
+        file = os.path.join(td, "log.txt")
+        program = str((Path(__file__).parent / "run_server.py").resolve().absolute())
+        command = [sys.executable, program, file, str(port)]
+
+        process = await stack.enter_async_context(
+            await anyio.open_process(command, start_new_session=True)
+        )
+        await stack.enter_async_context(cleanup_process(process))
+        # wait for the server to start
+        with anyio.fail_after(5):
+            await wait_for_server(port)
+
+        process.send_signal(signal.SIGINT)
+        with anyio.fail_after(5):
+            code = await process.wait()
+            assert code == 0
+
+        with open(file) as f:
+            assert f.read().split("\n") == ["start", "stop"]
